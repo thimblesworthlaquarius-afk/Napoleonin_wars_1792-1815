05b83b118e1d62588478a35229804c11255536f3
Napoleon Freakaparte <thimblesworthlaquarius@gmail.com>
2025-11-24 22:18:37 -0500

    Initial mod upload

---
+++ common/defines/REWORKED_graphics.lua
@@ -0,0 +1,10 @@
+-- REWORKED mod
+
+
+NDefines.NGraphics.GRADIENT_BORDERS_OUTLINE_CUTOFF_COUNTRY = 1.0
+NDefines.NGraphics.SUN_HEIGHT = 1500
+NDefines.NGraphics.SUN_INTENSITY = 0.9
+NDefines.NGraphics.MOON_HEIGHT = 1500
+NDefines.NGraphics.MOON_INTENSITY = 0.9
+NDefines_Graphics.NGraphics.BLOOM_SCALE = 0.0
+NDefines_Graphics.NGraphics.LAND_COMBAT_DISTANCE_CUTOFF = 500
